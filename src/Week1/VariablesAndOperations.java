65ff8ecd4031c9d5992b39191742656efdacbf83
fmhagan2 <fmhagan2@gmail.com>
2022-12-27 16:43:47 -0600

    first commit being made

---
+++ src/Week1/VariablesAndOperations.java
@@ -0,0 +1,21 @@
+package Week1;
+
+public class VariablesAndOperations {
+
+	public static void main(String[] args) {
+		
+		int availableSeatsOnFlight = 8;
+		double costOfGroceriesAtCheckout = 109.33;
+		char middleInitial = 'M';
+		boolean isHotOutside = true;
+		String customerFirstName = "Felicia";
+		String streetAddress = "124 Main St";
+		
+		System.out.println("Plane Seats Avavailable: " + availableSeatsOnFlight);
+		System.out.println("Grocery Total: " + costOfGroceriesAtCheckout);
+		System.out.println("Middle Initial: " + middleInitial);
+		System.out.println("It is hot outside: " + isHotOutside);
+		System.out.println("Customer First Name: " + customerFirstName);
+		System.out.println("Street Address: " + streetAddress);
+	}
+}
